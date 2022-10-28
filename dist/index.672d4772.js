e5a759fee23c6e13dc0642fcf6ccdb760d74effb
Evgen <yevgenchikk@mail.ru>
2022-10-28 14:23:36 +0300

    add eslint and parcel

---
+++ dist/index.672d4772.js
@@ -0,0 +1,7 @@
+const ELEMENTS = {
+    input: document.querySelector(".field__input"),
+    submit: document.querySelector(".field__button"),
+    output: document.querySelector(".output")
+};
+
+//# sourceMappingURL=index.672d4772.js.map
---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const ELEMENTS = {
+  input: document.querySelector('.field__input'),
+  submit: document.querySelector('.field__button'),
+  output: document.querySelector('.output')
+}
+
+
