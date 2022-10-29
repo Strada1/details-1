909e0a91402b89e3c3f8c27081bdd3dc68ec84d5
Dima <61251772+DimaSU2020@users.noreply.github.com>
2022-10-29 08:59:38 +0300

    bug fixes done

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
 import {
     formatDuration, intervalToDuration
   } from 'date-fns';
+import {ELEMENT} from './view';
 
-const formDate = document.querySelector('.form-date');
-const fromDate = document.querySelector('.from-date');
-const dateCountDown = document.querySelector('.date-count-down');
-
-formDate.addEventListener('submit', (event) => handlerCheckDate(event));
+ELEMENT.formDate.addEventListener('submit', (event) => handlerCheckDate(event));
 
 function handlerCheckDate(event) {
   event.preventDefault();
-  dateCountDown.classList.add('active');
-  const timeWeIntresting = fromDate.value;
+  const currentTime = new Date();
+  const timeWeIntresting = ELEMENT.fromDate.value;
+  if (!timeWeIntresting) {
+    return alert('Enter date, please!');
+  }
+  ELEMENT.textInform.classList.add('active');
+  ELEMENT.dateCountDown.classList.add('active');
+  ELEMENT.btnStart.setAttribute("disabled", "disabled");
+  if (currentTime > new Date(timeWeIntresting)) {
+    ELEMENT.textInform.textContent = 'From this date has passed';
+  } else {
+    ELEMENT.textInform.textContent = 'From this date to the future date';
+  }
   let timerId = setInterval(getCurrentTime, 1000, timeWeIntresting);
-  setTimeout(() => { clearInterval(timerId); dateCountDown.classList.remove('active');}, 10000);
+  setTimeout(() => {
+    ELEMENT.btnStart.removeAttribute("disabled", "disabled");
+    ELEMENT.dateCountDown.classList.remove('active');
+    ELEMENT.dateCountDown.textContent = '';
+    ELEMENT.textInform.classList.remove('active');
+    ELEMENT.fromDate.value = '';
+    clearInterval(timerId); 
+  }, 12000);
 };
 
 function getCurrentTime(timeWeIntresting) {
-  const currentTime = new Date();
   const differentDate = intervalToDuration({
     start: new Date(timeWeIntresting),
-    end: currentTime,
+    end: new Date(),
   })
   const myDifferentDay = formatDuration(differentDate);
-  dateCountDown.textContent = myDifferentDay;
+  ELEMENT.dateCountDown.textContent = myDifferentDay;
 };
---
+++ js/view.js
@@ -0,0 +1,9 @@
+export {ELEMENT};
+
+const ELEMENT = {
+    formDate: document.querySelector('.form-date'),
+    fromDate: document.querySelector('.from-date'),
+    dateCountDown: document.querySelector('.date-count-down'),
+    textInform: document.querySelector('.text-inform'),
+    btnStart: document.querySelector('.btn-start'),
+}
