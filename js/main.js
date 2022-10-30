387c1e64f24a1ee33ae1bb4d61d0cebf1b0ebb4a
Dima <61251772+DimaSU2020@users.noreply.github.com>
2022-10-30 10:25:16 +0300

    checked errors and add popup

--- js/main.js
+++ js/main.js
@@ -1,41 +1,53 @@
 import {
-    formatDuration, intervalToDuration
-  } from 'date-fns';
-import {ELEMENT} from './view';
+  formatDuration, intervalToDuration,
+} from 'date-fns';
+import {ELEMENT, ERRORS} from './view';
 
 ELEMENT.formDate.addEventListener('submit', (event) => handlerCheckDate(event));
+ELEMENT.btnClear.addEventListener('click', handlerClear);
+let myTimerId;
 
 function handlerCheckDate(event) {
   event.preventDefault();
   const currentTime = new Date();
-  const timeWeIntresting = ELEMENT.fromDate.value;
-  if (!timeWeIntresting) {
-    return alert('Enter date, please!');
+  const weIntrestingTime = ELEMENT.fromDate.value;
+  if (!weIntrestingTime) {
+    return ERRORS.empty_date();
   }
   ELEMENT.textInform.classList.add('active');
   ELEMENT.dateCountDown.classList.add('active');
-  ELEMENT.btnStart.setAttribute("disabled", "disabled");
-  if (currentTime > new Date(timeWeIntresting)) {
-    ELEMENT.textInform.textContent = 'From this date has passed';
+  if (currentTime > new Date(weIntrestingTime)) {
+    ELEMENT.textInform.textContent = 'How much time passed from this';
   } else {
     ELEMENT.textInform.textContent = 'From this date to the future date';
   }
-  let timerId = setInterval(getCurrentTime, 1000, timeWeIntresting);
-  setTimeout(() => {
-    ELEMENT.btnStart.removeAttribute("disabled", "disabled");
-    ELEMENT.dateCountDown.classList.remove('active');
-    ELEMENT.dateCountDown.textContent = '';
-    ELEMENT.textInform.classList.remove('active');
-    ELEMENT.fromDate.value = '';
-    clearInterval(timerId); 
-  }, 12000);
+  myTimer(weIntrestingTime);
 };
 
-function getCurrentTime(timeWeIntresting) {
+function myTimer(weIntrestingTime) {
+  eraserTimerId(myTimerId)
+  myTimerId = setInterval(getDifferentTime, 1000, weIntrestingTime);
+};
+
+function getDifferentTime(weIntrestingTime) {
   const differentDate = intervalToDuration({
-    start: new Date(timeWeIntresting),
+    start: new Date(weIntrestingTime),
     end: new Date(),
   })
   const myDifferentDay = formatDuration(differentDate);
   ELEMENT.dateCountDown.textContent = myDifferentDay;
+};
+
+function handlerClear() {
+  eraserTimerId(myTimerId);
+  ELEMENT.dateCountDown.classList.remove('active');
+  ELEMENT.textInform.classList.remove('active');
+  ELEMENT.textInform.value = '';
+  ELEMENT.fromDate.value = '';
+};
+
+function eraserTimerId(timerId) {
+  if (timerId) {
+    clearInterval(timerId);
+  }
 };
