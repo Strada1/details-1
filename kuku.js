c943f400d5c3f71f0ac4ce185ff00869872951ee
yarmkhlv <yarmkhlv@gmail.com>
2022-11-01 12:09:29 +0400

    airbnb bug fix test

--- kuku.js
+++ kuku.js
@@ -1,236 +1,214 @@
-import { format}  from './node_modules/date-fns'
-
+import { format } from 'date-fns/esm';
 
 window.addEventListener('load', startPage);
-//Блок поиска
-let search__form = document.querySelector('.search__form');
+// Блок поиска
+const search__form = document.querySelector('.search__form');
 search__form.addEventListener('submit', takeCity);
-
 
 const CurrentCity = localStorage.getItem('CurrentCity');
 
-
-let setCities = localStorage.getItem('CitiesSet'); 
+let setCities = localStorage.getItem('CitiesSet');
 setCities = JSON.parse(setCities);
 setCities = new Set(setCities);
 console.log(setCities);
 console.log(typeof setCities);
 
 function startPage() {
-    createRightBlock();
-    quickStart();
-    document.querySelector('.item-plus').click();
+  createRightBlock();
+  quickStart();
+  document.querySelector('.item-plus').click();
 }
 
 function takeCity(event) {
-    let search__input = document.querySelector('.search__input');
-    const cityName = search__input.value;
-    const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-    const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
-    
-    if (event) { 
-        event.preventDefault();
-    }
-    
-    async function a() {
-        try {
-            const response = await fetch(url);
-                
-                if (response.status === 400) {
-                    throw new EmptyRequestError('Пустой запрос')
-                }
-    
-                if (response.status === 404) {
-                    throw new InvalidRequsetError('Неккоректный запрос');
-                }
+  const search__input = document.querySelector('.search__input');
+  const cityName = search__input.value;
+  const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+  const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+  const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
 
-                const result =  await response.json();
-                render(result);
-                localStorage.setItem('CurrentCity', cityName);
-            }   
-        catch(error) {
-            alert(error.message);
-        };
-    }
-    a();
-};
+  if (event) {
+    event.preventDefault();
+  }
 
+  async function a() {
+    try {
+      const response = await fetch(url);
+
+      if (response.status === 400) {
+        throw new EmptyRequestError('Пустой запрос');
+      }
+
+      if (response.status === 404) {
+        throw new InvalidRequsetError('Неккоректный запрос');
+      }
+
+      const result = await response.json();
+      render(result);
+      localStorage.setItem('CurrentCity', cityName);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+  a();
+}
 
 function render(result) {
-    createFirstBlock(result);
-    createSecondBlock(result);
-    createRightBlock(result);
-};
+  createFirstBlock(result);
+  createSecondBlock(result);
+  createRightBlock(result);
+}
 
 function createFirstBlock(result) {
-    let cityNow = document.querySelector('.block__item-city');
-    let tempNow = document.querySelector('.block__item-temp');
-    let weatherImgNow = document.querySelector('.block__item-weather-img');
-    let likeNow = document.querySelector('.block__item-like');
-    cityNow.textContent = result.name;
-    console.log('result', result);
-    tempNow.textContent = Math.round(result.main.temp)+'°';
-    weatherImgNow.src = `http://openweathermap.org/img/wn/${result.weather[0].icon}@4x.png`;
-    likeNow.addEventListener('click', likeThisCity);
-
-};
+  const cityNow = document.querySelector('.block__item-city');
+  const tempNow = document.querySelector('.block__item-temp');
+  const weatherImgNow = document.querySelector('.block__item-weather-img');
+  const likeNow = document.querySelector('.block__item-like');
+  cityNow.textContent = result.name;
+  console.log('result', result);
+  tempNow.textContent = `${Math.round(result.main.temp)}°`;
+  weatherImgNow.src = `http://openweathermap.org/img/wn/${result.weather[0].icon}@4x.png`;
+  likeNow.addEventListener('click', likeThisCity);
+}
 
 function createSecondBlock(result) {
-    let cityDetails = document.querySelector('.selected__city-details');
-    let detailTemp = document.querySelector('.detail-temp');
-    let detailFeels = document.querySelector('.detail-feels');
-    let detailWeather = document.querySelector('.detail-weather');
-    let detailSunrise = document.querySelector('.detail-sunrise');
-    let detailSunset = document.querySelector('.detail-sunset');
-    cityDetails.textContent = result.name;
-    detailTemp.textContent = `Temperature: ${(Math.round(result.main.temp))} °`;
-    detailFeels.textContent = `Feels like: ${(Math.round(result.main.temp))} °`;
-    detailWeather.textContent = `Weather: ${result.weather[0].main}`;
-    let sunriseTime = new Date(showLocalSunrise(result));
-    let sunsetTime = new Date(showLocalSunset(result));
-    // const timeOptions = {
-    //     hour: "numeric",
-    //     minute : "2-digit"
-    // };
-    detailSunrise.textContent = `Sunrise: ${format((sunriseTime), 'h:mm aaa')}`;
-    detailSunset.textContent = `Sunset: ${format((sunsetTime), 'h:mm aaa')}`;
+  const cityDetails = document.querySelector('.selected__city-details');
+  const detailTemp = document.querySelector('.detail-temp');
+  const detailFeels = document.querySelector('.detail-feels');
+  const detailWeather = document.querySelector('.detail-weather');
+  const detailSunrise = document.querySelector('.detail-sunrise');
+  const detailSunset = document.querySelector('.detail-sunset');
+  cityDetails.textContent = result.name;
+  detailTemp.textContent = `Temperature: ${(Math.round(result.main.temp))} °`;
+  detailFeels.textContent = `Feels like: ${(Math.round(result.main.temp))} °`;
+  detailWeather.textContent = `Weather: ${result.weather[0].main}`;
+  const sunriseTime = new Date(showLocalSunrise(result));
+  const sunsetTime = new Date(showLocalSunset(result));
+  // const timeOptions = {
+  //     hour: "numeric",
+  //     minute : "2-digit"
+  // };
+  detailSunrise.textContent = `Sunrise: ${format((sunriseTime), 'h:mm aaa')}`;
+  detailSunset.textContent = `Sunset: ${format((sunsetTime), 'h:mm aaa')}`;
+}
 
-};
-
-//доп со временем
+// доп со временем
 function showLocalSunrise(result) {
-    const myOffset = new Date().getTimezoneOffset() * 60 * 1000;
-    const offsetReqCity = result.timezone * 1000;
-    const sunrise = result.sys.sunrise * 1000;
-    const localSunrise = sunrise + myOffset + offsetReqCity;
-    return localSunrise;
-};
+  const myOffset = new Date().getTimezoneOffset() * 60 * 1000;
+  const offsetReqCity = result.timezone * 1000;
+  const sunrise = result.sys.sunrise * 1000;
+  const localSunrise = sunrise + myOffset + offsetReqCity;
+  return localSunrise;
+}
 function showLocalSunset(result) {
-    const myOffset = new Date().getTimezoneOffset() * 60 * 1000;
-    const offsetReqCity = result.timezone * 1000;
-    const sunset = result.sys.sunset * 1000;
-    const showLocalSunset = sunset + myOffset + offsetReqCity;
-    return showLocalSunset;
-};
-
+  const myOffset = new Date().getTimezoneOffset() * 60 * 1000;
+  const offsetReqCity = result.timezone * 1000;
+  const sunset = result.sys.sunset * 1000;
+  const showLocalSunset = sunset + myOffset + offsetReqCity;
+  return showLocalSunset;
+}
 
 const myTime = new Date();
 const myTime2 = myTime.getTimezoneOffset();
-const myTime3 = myTime - myTime2*60*1000;
-const timeTokyo = 540*60*1000;
-const localTokyo =  new Date(myTime3 - timeTokyo);
+const myTime3 = myTime - myTime2 * 60 * 1000;
+const timeTokyo = 540 * 60 * 1000;
+const localTokyo = new Date(myTime3 - timeTokyo);
 console.log(localTokyo);
 
-
-
-
-let createThirdBlock = function(result) {
-    let cityForecast = document.querySelector('.selected__city-forecast');
+const createThirdBlock = function (result) {
+  const cityForecast = document.querySelector('.selected__city-forecast');
 };
 
 function createRightBlock() {
-    clear();
-    setCities.forEach(elem => {
-        createCityElement(elem)
-    })
-};
-
-
+  clear();
+  setCities.forEach((elem) => {
+    createCityElement(elem);
+  });
+}
 
 function likeThisCity() {
-    let cityNow = document.querySelector('.block__item-city');
-    setCities.add(cityNow.textContent);
-    createRightBlock();
-    localStorage.setItem('CitiesSet', JSON.stringify(Array.from(setCities)));
+  const cityNow = document.querySelector('.block__item-city');
+  setCities.add(cityNow.textContent);
+  createRightBlock();
+  localStorage.setItem('CitiesSet', JSON.stringify(Array.from(setCities)));
+}
+
+// -------------- Побочные функции
+let createCityElement = function (item) {
+  const likedArea = document.querySelector('.liked__locations');
+  const miniBlockCity = document.createElement('div');
+  miniBlockCity.classList.add('added__list');
+  const selectedCity = document.createElement('p');
+  selectedCity.classList.add('added__list__item');
+  selectedCity.textContent = item;
+  selectedCity.addEventListener('click', quickRequest(item));
+  const selectedCityButton = document.createElement('button');
+  selectedCityButton.classList.add('added__list__button');
+  selectedCityButton.addEventListener('click', quickRemove);
+  selectedCityButton.innerHTML = '&times;';
+  miniBlockCity.append(selectedCity, selectedCityButton);
+  likedArea.append(miniBlockCity);
 };
 
-
-
-
-
-//-------------- Побочные функции
-let createCityElement = function(item) {
-    let likedArea = document.querySelector('.liked__locations');
-    let miniBlockCity = document.createElement('div');
-        miniBlockCity.classList.add('added__list');
-    let selectedCity = document.createElement('p');
-        selectedCity.classList.add('added__list__item');
-        selectedCity.textContent = item;
-        selectedCity.addEventListener('click', quickRequest(item));
-    let selectedCityButton = document.createElement('button');
-        selectedCityButton.classList.add('added__list__button');
-        selectedCityButton.addEventListener('click', quickRemove);
-        selectedCityButton.innerHTML = "&times;";     
-        miniBlockCity.append(selectedCity, selectedCityButton);
-        likedArea.append(miniBlockCity);    
+let quickRequest = (text) => {
+  const innerText = text;
+  return function () {
+    const search__input = document.querySelector('.search__input');
+    // let target = event.target;
+    search__input.value = innerText;
+    takeCity();
+  };
 };
 
-let quickRequest = (text) => { 
-    const innerText = text; 
-    return function() {
-        let search__input = document.querySelector('.search__input');
-        // let target = event.target;
-        search__input.value = innerText;
-        takeCity();
-    }
-};
-
-let quickRemove = function() {
-    let target =  event.target;
-    target = target.previousSibling.textContent;
-    setCities.delete(target);
-    createRightBlock();
-    localStorage.setItem('CitiesSet', JSON.stringify(Array.from(setCities)));
+let quickRemove = function () {
+  let { target } = event;
+  target = target.previousSibling.textContent;
+  setCities.delete(target);
+  createRightBlock();
+  localStorage.setItem('CitiesSet', JSON.stringify(Array.from(setCities)));
 };
 
 function quickStart() {
-    let search__input = document.querySelector('.search__input');
-    const cityName = CurrentCity;
-    const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-     const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
-        fetch(url)
-            .then(response => response.json())
-            .then(result => {render(result);
-                localStorage.setItem('CurrentCity', cityName);})
-            .catch(err => alert(err.message));
-};
-
-
-let clear = function() {
-    let liked__locations = document.querySelector('.liked__locations');
-    while (liked__locations.firstChild) {
-        liked__locations.removeChild(liked__locations.firstChild)
-    };}
-
-
-
-
-//Ошибки
-class MyError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = this.constructor.name;
-    }
-};
-
-class EmptyRequestError extends MyError {
-    constructor(message) {
-        super(message);
-    }
-};
-class InvalidRequestError extends MyError {
-    constructor(message) {
-        super(message);
-    }
+  const search__input = document.querySelector('.search__input');
+  const cityName = CurrentCity;
+  const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+  const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+  const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      render(result);
+      localStorage.setItem('CurrentCity', cityName);
+    })
+    .catch((err) => alert(err.message));
 }
 
+let clear = function () {
+  const liked__locations = document.querySelector('.liked__locations');
+  while (liked__locations.firstChild) {
+    liked__locations.removeChild(liked__locations.firstChild);
+  }
+};
 
+// Ошибки
+class MyError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
 
+class EmptyRequestError extends MyError {
+  constructor(message) {
+    super(message);
+  }
+}
+class InvalidRequestError extends MyError {
+  constructor(message) {
+    super(message);
+  }
+}
 
-
-/*Рекурсия
+/* Рекурсия
 function checkChildren(elem, sum, deep, result) {
     const tab = Array(deep).fill('--').join();
     let deep2 = deep+1;
