d1f2bcafdd0ee215ef6a764cbff706d7d86458dd
Dima <61251772+DimaSU2020@users.noreply.github.com>
2022-11-06 15:52:04 +0300

    fixed bugs and optimized the code

--- js/main.js
+++ js/main.js
@@ -1,47 +1,40 @@
 class Storage {
-    constructor(key, websiteStorage = "local") {
+    constructor(key, typeOfStorage = "local") {
         this.key = key;
-        this.store = [];
-        this.websiteStorage = websiteStorage;
-        this.websiteStorage === 'session'
-        ? this._myStorage = sessionStorage
-        : this._myStorage = localStorage;
-    }
-    workWithStorageData (value) {
-        !value
-        ? this.store = []
-        : this.store = value
-        const dataStorage = JSON.stringify(this.store);
-        this._myStorage.setItem(this.key, dataStorage);
+        this.typeOfStorage = typeOfStorage === 'session'
+        ? sessionStorage
+        : localStorage;
     }
     set (value) {
-        this.workWithStorageData (value);
+        this.value = value;
+        this.typeOfStorage.setItem(this.key, JSON.stringify(this.value));
     }
     get () {
-        this.store = JSON.parse(this._myStorage.getItem(this.key))
+        return this.value = JSON.parse(this.typeOfStorage.getItem(this.key))
     }
     clear () {
-        this.workWithStorageData ();
+        this.value = [];
+        this.typeOfStorage.setItem(this.key, JSON.stringify(this.value));
     }
     delete () {
-        this._myStorage.removeItem(this.key)
+        this.typeOfStorage.removeItem (this.key)
     }
     isEmpty () {
-        this.store = JSON.parse(this._myStorage.getItem(this.key))
-        if (!this.store) {
+        this.value = this.get ();
+        if (!this.value) {
             return true;
         }
-        if (this.store.length === 0) {
+        if (this.value.length === 0) {
             return true;
         }
         return false;
     }
 }
 
-let key = 'Ann';
-const websiteStorage = 'local';
-const dataLesson26 = ['iPhone 14Pro', 'Apple Whatch8'];
-const names = new Storage(key, websiteStorage);
+let key = 'John';
+const dataLesson26 = ['iPhone 14Pro', 'Apple Watch 8', 'MacBook 16 Pro'];
+const typeOfStorage = 'local';
+const names = new Storage(key, typeOfStorage);
 console.log(names);
 names.set(dataLesson26);
 console.log(names.get());
