78db5b1ef18525daca1a8528f3948bb47674d1fa
Jarvis-DeNero <github.nikiforov94@gmail.com>
2022-11-06 21:40:59 +0200

    #26 - нейминг в классе Storage

--- JarvisDeNero/classes/Storage.js
+++ JarvisDeNero/classes/Storage.js
@@ -1,21 +1,21 @@
 class Storage {
-   constructor(names, isLocalStorage = true) {
-      this.names = names;
+   constructor(name, isLocalStorage = true) {
+      this.name = name;
       this._storage = isLocalStorage ? localStorage : sessionStorage;
    }
    get() {
-      return JSON.parse(this._storage.getItem(this.names));
+      return JSON.parse(this._storage.getItem(this.name));
    }
    set(value) {
-      this._storage.setItem(this.names, JSON.stringify(value));
+      this._storage.setItem(this.name, JSON.stringify(value));
       console.log('Added!');
    }
    clear() {
-      this._storage.removeItem(this.names);
+      this._storage.removeItem(this.name);
       console.log('Deleted!');
    }
    isEmpty() {
-      return Boolean(this._storage.getItem(this.names));
+      return Boolean(this._storage.getItem(this.name));
    }
 }
 
