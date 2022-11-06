6ca7c9a1c98fa133cd95be6ccb479fa6dd3631a7
Jarvis-DeNero <github.nikiforov94@gmail.com>
2022-11-06 18:48:18 +0200

    #26 класс Storage 1.1 - внёс изменения в ксласс Storage

--- JarvisDeNero/classes/Storage.js
+++ JarvisDeNero/classes/Storage.js
@@ -1,31 +1,21 @@
 class Storage {
-   constructor(names, isSessionStorage = false) {
+   constructor(names, isLocalStorage = true) {
       this.names = names;
-      this.isSessionStorage = isSessionStorage;
+      this._storage = isLocalStorage ? localStorage : sessionStorage;
    }
    get() {
-      if (this.isSessionStorage) {
-         return JSON.parse(sessionStorage.getItem(this.names));
-      }
-      return JSON.parse(localStorage.getItem(this.names));
+      return JSON.parse(this._storage.getItem(this.names));
    }
    set(value) {
-      this.isSessionStorage ?
-         sessionStorage.setItem(this.names, JSON.stringify(value)) :
-         localStorage.setItem(this.names, JSON.stringify(value));
+      this._storage.setItem(this.names, JSON.stringify(value));
       console.log('Added!');
    }
    clear() {
-      this.isSessionStorage ?
-         sessionStorage.removeItem(this.names) :
-         localStorage.removeItem(this.names);
+      this._storage.removeItem(this.names);
       console.log('Deleted!');
    }
    isEmpty() {
-      if (this.isSessionStorage) {
-         return Boolean(sessionStorage.getItem(this.names));
-      }
-      return Boolean(localStorage.getItem(this.names));
+      return Boolean(this._storage.getItem(this.names));
    }
 }
 
