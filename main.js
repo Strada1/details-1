77f227e9a9f596ba8a8540519bfdd5a3dd1bc04d
pipupopi <sonalavrushin@gmail.com>
2022-11-07 23:06:24 +0300

    pipupopi/details-5

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
-const localОption = {
-  "localStorage" : localStorage, 
-  "sessionStorage" : sessionStorage,
-}
-
-
 class Storage {
-    constructor(key = "defaultKey", value = 'defaultValue', defaultLocalStorage = localОption.localStorage) {
-      this.key = key; 
-      this.value = value; 
-      this.storage = defaultLocalStorage
-    }
-    get() {
-        return JSON.parse(this.storage.getItem(this.key));
-    }
-    set() {
-      this.storage.setItem(this.key, JSON.stringify(this.value));
-    }
-    clear() {
-      this.storage.removeItem(this.key)
-    }
-    isEmpty() {
-      return !this.get() ? true : false
-    }
-  };
+  constructor(key, local) {
+    this.key = key ?? "defaultKey",
+    this.storage = local ?? localStorage
+  }
+  get() {
+    return JSON.parse(this.storage.getItem(this.key));
+  }
+  set(value) {
+    this.storage.setItem(this.key, JSON.stringify(value ?? 'defaultValue'));
+  }
+  clear() {
+    this.storage.removeItem(this.key);
+  }
+  isEmpty() {
+    return this.get() ? false : true;
+  }
+}
