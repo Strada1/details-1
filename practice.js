88e1334052da0f56eeea93c0e0e149d4b815afe0
Dariannyko <dariannyko@gmail.com>
2022-11-07 23:21:40 +0300

    refractoring

--- practice.js
+++ practice.js
@@ -1,27 +1,32 @@
-const storageType = {
-  local: localStorage,
-  session: sessionStorage,
-};
-
 class Storage {
-  constructor(names = "users", storageName = storageType.local) {
-    this.names = names;
-    this.storageName = storageName;
-  }
-
-  get() {
-    return this.storageName.getItem(this.names);
+  constructor(names, storageName = localStorage) {
+    this.names = names ?? "names";
+    this.storageName = storageName === sessionStorage ? sessionStorage : localStorage;
   }
 
   set(value) {
-    this.storageName.setItem(this.names, value);
+    this.storageName.setItem(this.names, JSON.stringify(value));
   }
 
-  clear() {
+  get() {
+    return JSON.parse(this.storageName.getItem(this.names));
+  }
+
+  delete() {
     this.storageName.removeItem(this.names);
   }
 
+  clear() {
+    this.set('');
+  }
+
   isEmpty() {
-    return this.get() === null || this.get() === undefined ? true : false;
+    return !this.get() ? true : false;
   }
 }
+
+const newNames = new Storage('John', sessionStorage);
+
+newNames.set(['MacBook', 'AirPods', 'Ipad']);
+newNames.clear();
+newNames.isEmpty();
