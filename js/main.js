b20ae75f13fa31f2d1091398134505a51d9e11fd
Dima <61251772+DimaSU2020@users.noreply.github.com>
2022-11-08 20:39:02 +0300

    last release

--- js/main.js
+++ js/main.js
@@ -1,43 +1,41 @@
 class Storage {
-    constructor(key, typeOfStorage = "local") {
+    constructor(key, typeStorage = 'local') {
         this.key = key;
-        this.typeOfStorage = typeOfStorage === 'session'
+        this.typeStorage = typeStorage === 'session'
         ? sessionStorage
         : localStorage;
     }
     set (value) {
-        this.value = value;
-        this.typeOfStorage.setItem(this.key, JSON.stringify(this.value));
+        this.value = value ?? [];
+        this.typeStorage.setItem (this.key, JSON.stringify(this.value));
     }
     get () {
-        return this.value = JSON.parse(this.typeOfStorage.getItem(this.key))
+        return this.value = JSON.parse (this.typeStorage.getItem(this.key))
     }
     clear () {
         this.value = [];
-        this.typeOfStorage.setItem(this.key, JSON.stringify(this.value));
+        this.typeStorage.setItem (this.key, JSON.stringify(this.value));
     }
     delete () {
-        this.typeOfStorage.removeItem (this.key)
+        this.typeStorage.removeItem (this.key)
     }
     isEmpty () {
         this.value = this.get ();
-        if (!this.value) {
-            return true;
-        }
-        if (this.value.length === 0) {
-            return true;
-        }
-        return false;
+        const trueFalse = this.value?.length ?? 0;
+        return Boolean(!trueFalse);
     }
 }
 
-let key = 'John';
 const dataLesson26 = ['iPhone 14Pro', 'Apple Watch 8', 'MacBook 16 Pro'];
-const typeOfStorage = 'local';
-const names = new Storage(key, typeOfStorage);
-console.log(names);
-names.set(dataLesson26);
-console.log(names.get());
-// names.clear();
-// names.delete();
-console.log(names.isEmpty(key));
+// const dataLesson26 = [];
+let key = 'John';
+const typeStorage = 'local';
+const user = new Storage(key, typeStorage);
+console.log(user);
+user.set(dataLesson26);
+console.log(user.get());
+// user.clear();
+// user.delete();
+console.log(user.isEmpty(key));
+
+
