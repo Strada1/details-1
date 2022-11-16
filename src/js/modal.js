7f9ec5aef3754b4b3f8b56855f9f3d85da8ad232
batyril <237x237@gmail.ru>
2022-11-16 19:57:39 +0300

    made the layout

---
+++ src/js/modal.js
@@ -0,0 +1,41 @@
+const UI = {
+  MODAl_TRIGGERS: document.querySelectorAll("[data-modal]"),
+  MODAl_CLOSE: document.querySelector("[data-modal-close]"),
+  MODAL: document.querySelector(".modal"),
+  SETTINGS: document.querySelector(".chat__settings"),
+};
+
+UI.SETTINGS.addEventListener("click", openModal);
+UI.MODAl_CLOSE.addEventListener("click", closeModal);
+function openModal() {
+  UI.MODAL.classList.add("show");
+  UI.MODAL.classList.remove("hide");
+  document.body.style.overflow = "hidden";
+  //отмена скорола
+}
+
+function closeModal() {
+  UI.MODAL.classList.add("hide");
+  UI.MODAL.classList.remove("show");
+  document.body.style.overflow = "scroll";
+  //возврат скорола
+}
+
+UI.MODAl_CLOSE.addEventListener("click", () => closeModal());
+
+//закрытые при нажатии вне
+UI.MODAL.addEventListener("click", (event) => {
+  const checkClicksOutside = event.target.classList.contains("modal");
+  if (checkClicksOutside) {
+    closeModal();
+  }
+});
+
+//закрытые при нажатии на кнопку
+document.addEventListener("keydown", (event) => {
+  const checkPushEscape =
+    UI.MODAL.classList.contains("show") && event.code === "Escape";
+  if (checkPushEscape) {
+    closeModal();
+  }
+});
