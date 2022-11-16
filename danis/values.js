806c2b569e6e1e0f66b905c2b01af48f4ea1cfc9
hollywood787 <hollywood7878@yandex.ru>
2022-11-16 15:33:05 +0300

    Verstka chata

---
+++ danis/values.js
@@ -0,0 +1,16 @@
+const settingsButton = document.querySelector('.hero__block-buttons-head-setings button');
+const closeButton = document.querySelector('.popup__buttons_head-close button');
+const popup = document.querySelector('.popup');
+const inputWriteMessage = document.querySelector('.hero__block-buttons-bottom-input input');
+const windowChat = document.querySelector('.hero__block_window-chat');
+const mainForm = document.querySelector('.hero__block-form');
+
+
+export {
+    settingsButton,
+    closeButton,
+    popup,
+    inputWriteMessage,
+    windowChat,
+    mainForm,
+}
