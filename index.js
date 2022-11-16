2160cb0e97a3025e4fdb7087b83b447b84373241
kamilsalikhov75 <kamilsalikhov75@gmail.com>
2022-11-16 11:06:12 +0300

    #38 Чат: верстка

---
+++ index.js
@@ -0,0 +1,28 @@
+const ELEMENT = {
+    POPUP: document.querySelector(".popup"),
+    POPUP_CLOSE_BUTTONS: document.querySelectorAll(".popup__button"),
+    BUTTONS: document.querySelectorAll(".chat__button"),
+};
+const popupButtons = [
+    "Настройки",
+    "Получить код"
+];
+
+function showPopup() {
+    ELEMENT.POPUP.classList.add("popup--active");
+}
+
+function closePopup() {
+    ELEMENT.POPUP.classList.remove("popup--active");
+}
+
+
+ELEMENT.BUTTONS.forEach(button => {
+    if (popupButtons.includes(button.textContent)) {
+        button.addEventListener("click", showPopup);
+    }
+});
+
+ELEMENT.POPUP_CLOSE_BUTTONS.forEach(button => {
+    button.addEventListener("click", closePopup);
+});
