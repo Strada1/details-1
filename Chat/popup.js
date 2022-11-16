4853b0b46199fb5366f471ba034e28420ce041cc
meVarensev <vitalik.unit@gmial.com>
2022-11-16 17:29:19 +0500

    Чат верстка

---
+++ Chat/popup.js
@@ -0,0 +1,23 @@
+const popupBg = document.querySelector('.popup__bg');
+const popup = document.querySelector('.popup');
+const openPopupButton = document.querySelector('#setting');
+const closePopupButton = document.querySelector('.close-popup');
+
+openPopupButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+
+closePopupButton.addEventListener('click',() => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
+
