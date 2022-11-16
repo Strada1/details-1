c6c1165282401317987187fdbfa022465150f9ff
AlinaGost <alinka14011998@mail.ru>
2022-11-16 15:34:35 +0400

    page layout 2.0 and 2.3

---
+++ main.js
@@ -0,0 +1,14 @@
+const settingsBtn = document.querySelector('#settings');
+const settingsModal = document.querySelector('#modal_settings');
+const closeModal = document.querySelector('#close_modal');
+
+settingsBtn.addEventListener('click', () => {
+  settingsModal.classList.remove('display_none');
+  settingsModal.classList.add('display_flex');
+});
+
+closeModal.addEventListener('click', () => {
+  settingsModal.classList.remove('display_flex');
+  settingsModal.classList.add('display_none');
+});
+
