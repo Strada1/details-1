efce7dcc742c2e7c81f677dd9017d737664eb8c1
PushToTalk <x_killer90@mail.ru>
2022-11-17 13:08:11 +0300

    Adding setmessages and scroll

--- Chat/src/index.js
+++ Chat/src/index.js
@@ -1,6 +1,45 @@
+//TODO:
+//Когда мало сообщений, прилепить их к низу;
+//Реализовать автоматическую прокрутку к началу при
+//добавлении сообщения;
+
+import { getHours, getMinutes } from 'date-fns';
+
+const chat = document.querySelector('#chat');
 const openSettings = document.querySelector('#open_settings');
 const settings = document.querySelector('#settings');
 const closeSettings = document.querySelector('#close_settings');
+const sentMessage = document.querySelector('#sentMessage');
+const message = document.querySelector('#message');
+
+const myName = 'Я';
+const heName = 'Мой собеседник';
+
+function setMessage(name, text) {
+  const temp = message.content.cloneNode(true);
+  const fetchIsTrue = false;
+
+  if (name === myName) {
+    temp.querySelector('div').classList.add('me');
+  } else {
+    temp.querySelector('div').classList.add('he');
+  }
+
+  if (fetchIsTrue) {
+    temp.querySelector('.message').classList.add('delivered');
+  } else {
+    temp.querySelector('.message').classList.add('sent');
+  }
+
+  const hours = getHours(Date.now());
+  const minutes = getMinutes(Date.now());
+  const time = `${hours}:${minutes}`;
+
+  temp.querySelector('p').textContent = `${name}:${text}`;
+  temp.querySelector('span').textContent = time;
+
+  chat.appendChild(temp);
+}
 
 openSettings.addEventListener('click', (event) => {
   event.preventDefault();
@@ -10,4 +49,13 @@ openSettings.addEventListener('click', (event) => {
 closeSettings.addEventListener('click', (event) => {
   event.preventDefault();
   settings.classList.remove('active');
+});
+
+sentMessage.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let textMessage = sentMessage.children[0].value;
+  if (textMessage.length > 0) {
+    setMessage(myName, textMessage);
+    sentMessage.children[0].value = '';
+  }
 });
