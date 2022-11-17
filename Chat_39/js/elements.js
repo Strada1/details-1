8d25f25e10de9e9bc79157b8f76c07d35c528ef5
Inal Lukyaev <lukyaevinal@gmail.com>
2022-11-17 22:56:10 +0300

    add task #39

---
+++ Chat_39/js/elements.js
@@ -0,0 +1,10 @@
+export const ELEMENTS = {
+  SETTINGS_WRAPPER: document.querySelector('.settings_wrapper'),
+  BTN_SETTINGS: document.querySelector('.btn-settings'),
+  CLOSE_WINDOW: document.querySelector('.close-window'),
+  MAIN_BLOCK: document.querySelector('.main-block'),
+  FORM: document.querySelector('form'),
+  MY_MESSAGE_INPUT: document.querySelector('.my_message-input'),
+  TEMPLATE_MY_MESSAGE: document.getElementById('template_my_message'),
+  MESSAGE_WINDOW: document.querySelector('.message-window'),
+};
