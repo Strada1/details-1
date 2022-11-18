f2245a4066c9719ec5b3564e9c1abf09f1c8051b
Kirill108 <kirill108bol@gmail.com>
2022-11-18 13:27:19 +0100

    ready

---
+++ js/authorization.js
@@ -0,0 +1,22 @@
+import { POPUP } from "./view"
+
+POPUP.GET_COD.addEventListener('click', sendCod)
+
+async function sendCod(event) {
+	event.preventDefault();
+	const userEmail = POPUP.INPUT.value.trim();
+
+	const response = await fetch('https://edu.strada.one/api/user', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8'
+		  },
+		body: JSON.stringify({email: `${userEmail}`})
+	});
+
+	const result = await response.json();
+	console.log('result: ', result);
+
+	console.log('response: ', response.ok);
+}
+
---
+++ js/view.js
@@ -0,0 +1,21 @@
+export const ELEMENT = {
+	SETTINGS: document.querySelector('.settings'),
+	AUTHORIZATION: document.querySelector('.authorization'),
+	SEND_MESSAGE: document.querySelector(".chat__form"),
+	INPUT_MESSAGE: document.querySelector(".input__message"),
+	TEXT_MESSAGE: document.querySelector(".text__my__SMS"),
+	CHAT_CONTAINER: document.querySelector(".chat__Container"),
+	TEMPLATE: document.getElementById("template"),
+	SCROl: document.querySelector(".container-scroll"),
+}
+
+export const POPUP = {
+	GET_COD: document.querySelector('.popup__authorization__button'),
+	INPUT: document.querySelector('.popup__authorization__input'),
+	CLOSE_SETTINGS: document.querySelector('.popup__setings__button__exit'),
+	CLOSE_AUTHORIZATION: document.querySelector('.popup__authorization__button__exit'),
+	SETTINGS: document.getElementById("popupSetings"),
+	AUTHORIZATION: document.getElementById("popupAuthorization"),
+	CONFIRMATION: document.getElementById("popupConfirmation"),
+	CLOSE_CONFIRMATION: document.querySelector('.popup__confirmation__button__exit'),
+}
