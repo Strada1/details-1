2942b3ff0f19596dd22baacf97c413318d60e860
Kai <azrailkai@gmail.com>
2022-11-19 22:51:51 +0600

    add new file for elements variable, create function addMessage

---
+++ js/elements.js
@@ -0,0 +1,6 @@
+export const ELEMENTS = {
+  MESSAGE_FORM: document.querySelector('#messageForm'),
+  MESSAGE_INPUT: document.querySelector('#messageInput'),
+  MESSAGE_TEMPLATE: document.querySelector('#messageTemplate'),
+  MESSAGE_LIST: document.querySelector('#messageList'),
+};
--- js/index.js
+++ js/index.js
@@ -1,21 +1,32 @@
-const ELEMS = {
-  MESSAGE_FORM: document.querySelector('#messageForm'),
-  MESSAGE_INPUT: document.querySelector('#messageInput'),
-};
+import { ELEMENTS } from './elements';
+import { getHours, getMinutes } from 'date-fns';
 
-ELEMS.MESSAGE_FORM.addEventListener('submit', (event) => addMessage(event));
+let user = 'Kai';
+
+ELEMENTS.MESSAGE_FORM.addEventListener('submit', (event) => addMessage(event));
 
 function addMessage(event) {
   event.preventDefault();
-  const messagevalue = checkMessage(ELEMS.MESSAGE_INPUT.value.trim());
-  console.log(messagevalue);
-  console.log(event, 'submit');
-}
+  const messagevalue = ELEMENTS.MESSAGE_INPUT.value.trim();
+  const hour = getHours(new Date());
+  const minute = getMinutes(new Date());
+  const messageDate = `${hour}:${minute}`;
+  if (messagevalue.length) {
+    console.log(ELEMENTS.MESSAGE_TEMPLATE);
+    const template = ELEMENTS.MESSAGE_TEMPLATE.content.cloneNode(true);
+    template.querySelector('.author').textContent = `${user}`;
+    template.querySelector('.message-text').textContent = messagevalue;
+    template.querySelector('.message-date').textContent = messageDate;
 
-function checkMessage(text) {
-  if (text !== null && typeof text !== 'undefined' && text !== '') {
-    return text;
-  } else {
-    alert('Введите сообщение!');
+    ELEMENTS.MESSAGE_LIST.append(template);
+    ELEMENTS.MESSAGE_INPUT.value = '';
   }
 }
+
+// function checkMessage(text) {
+//   if (text !== null && typeof text !== 'undefined' && text !== '') {
+//     return text;
+//   } else {
+//     alert('Введите сообщение!');
+//   }
+// }
