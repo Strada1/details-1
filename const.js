7db4d74450dee854ab76082966d1ba9f76c82107
kamilsalikhov75 <kamilsalikhov75@gmail.com>
2022-11-20 11:56:19 +0300

    #41 Чат: смена имени

---
+++ const.js
@@ -0,0 +1,55 @@
+export const ELEMENT = {
+	POPUP: document.querySelector(".popup"),
+	POPUP_CLOSE_BUTTONS: document.querySelectorAll(".popup__button"),
+	SETTING_BUTTON: document.getElementById("settingsButton"),
+	EXIT_BUTTON: document.getElementById("exitButton"),
+	MESSAGES: document.querySelector(".chat__main"),
+	TEMPLATE: document.getElementById("template"),
+	SEND_MESSAGE_FORM: document.getElementById("sendMessageForm"),
+	MESSAGE_INPUT: document.getElementById("sendMessageForm").querySelector(".chat__input"),
+	CONTAINER: document.querySelector(".container"),
+	CHAT: document.querySelector(".chat__inner")
+};
+
+export const POPUP = {
+	SETTINGS: {
+		TITLE: "Настройки",
+		INPUT_TITLE: "Имя в чате",
+		BUTTON: "->",
+		FORM_ID: "popupSettings",
+		CLOSE_BUTTON_ID: "closeSetting",
+		INPUT_TYPE: "text"
+	},
+	AUTHORIZATION: {
+		TITLE: "Авторизация",
+		INPUT_TITLE: "Почта",
+		BUTTON: "Получить код",
+		FORM_ID: "popupAuthorization",
+		CLOSE_BUTTON_ID: "closeAuthorization",
+		INPUT_TYPE: "email"
+	},
+	CONFIRMATION: {
+		TITLE: "Подтверждение",
+		INPUT_TITLE: "Код",
+		BUTTON: "Войти",
+		FORM_ID: "popupConfirmation",
+		CLOSE_BUTTON_ID: "closeConfirmation",
+		INPUT_TYPE: "text"
+	}
+};
+
+export const classes = {
+	chatDisable: "chat__inner--disabled"
+};
+
+export const URL = {
+	USER: "https://edu.strada.one/api/user",
+	USER_ME: "https://edu.strada.one/api/user/me"
+};
+
+export const USER = {
+	ME: {
+		TOKEN: "",
+		NAME: "",
+	}
+};
