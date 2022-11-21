fb7c935b671590c6baa3b3a5c924af129957a451
Alexey Rusakov <alexeyrusakov1999@gmail.com>
2022-11-21 14:08:37 +0300

    add app

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log("Hello world!");
+const ELEMENTS = {
+  input: document.querySelector(".input"),
+  date: document.querySelector(".date-distance p"),
+  buttonResult: document.querySelector(".button"),
+};
+
+function dateDistance() {
+  const currentDate = Date.parse(ELEMENTS.input.value);
+  const totalTime = currentDate - new Date();
+  const resultTime = new Date(totalTime);
+
+  return { totalTime, resultTime };
+}
+
+function render(time) {
+  let timeStroke;
+  time === "Invalid Date"
+    ? (ELEMENTS.date.textContent = "Вы ввели некорректную дату")
+    : (timeStroke = `${time.getFullYear() - 1970} лет
+    ${time.getUTCMonth()} мес
+    ${time.getUTCDate() - 1} дн
+    ${time.getUTCHours()} ч 
+    ${time.getUTCMinutes()} мин 
+    ${time.getUTCSeconds()} сек`);
+
+  ELEMENTS.date.textContent = timeStroke;
+}
+
+function interval() {
+  const timerId = setInterval(() => {
+    const result = dateDistance();
+    if (result.totalTime <= 0) {
+      clearInterval(timerId);
+    } else {
+      console.log(result.resultTime);
+      render(result.resultTime);
+    }
+  }, 1000);
+}
+
+ELEMENTS.buttonResult.addEventListener("click", (event) => {
+  interval();
+  event.preventDefault();
+});
