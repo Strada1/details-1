4b8343fa640517197522bb562ec8aac063df6d57
YaRolly <Ea4GoodRu@gmail.com>
2022-11-30 00:38:38 +0300

    Practice TS:introduction v1.0

---
+++ const.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+exports.SYMBOL = exports.ELEMENTS = void 0;
+exports.ELEMENTS = {
+    FIRST_NUMBER: document.querySelector('#first-number'),
+    SECOND_NUMBER: document.querySelector('#second-number'),
+    CALC: document.querySelector('#calc'),
+    EQUALS: document.querySelector('#btn'),
+    RESULT: document.querySelector('#result')
+};
+exports.SYMBOL = {
+    ADD: '+',
+    SUB: '-',
+    DEL: '/',
+    MULTI: '*'
+};
