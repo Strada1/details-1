00e06fba6bbc10f151d2261f1754dcd054524566
pipupopi <sonalavrushin@gmail.com>
2022-12-11 21:35:13 +0300

    ну вроде что-то вышло. чат на тс

--- js/cookie.js
+++ js/cookie.js
@@ -1,21 +1,20 @@
 import Cookies from "js-cookie";
-
 export function cookieSet(key, value) {
-  try{
-  Cookies.set(key, JSON.stringify(value));
-  } catch(error) {
-    console.log(error.message)
-  }
-}
-
-export function cookieGet(key) {
-  try{
-    if (Cookies.get(key) == null) {
-      return false;
-    } else {
-      return JSON.parse(Cookies.get(key));
+    try {
+        Cookies.set(key, JSON.stringify(value));
     }
-  } catch(error) {
-    console.log(error.message)
-  }
-  }
+    catch (error) {
+        console.log(error);
+    }
+}
+export function cookieGet(key) {
+    const jsonType = Cookies.get(key);
+    try {
+        if (jsonType) {
+            return JSON.parse(jsonType);
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
--- js/popup.js
+++ js/popup.js
@@ -1,43 +1,34 @@
-import { SETTINGS, CONFIRMATION, ELEMENTS, AUTHORIZATION } from "./const.js";
-import { deleteAccountHistory } from "./chat.js";
-
-//ментор, не стреляй в колени пж, да это плохо, что сток обработчиков, но оно работает...
-//сделала из своих слез
-
+import { SETTINGS, CONFIRMATION, ELEMENTS, AUTHORIZATION } from "./const";
+import { deleteAccountHistory } from "./chat";
 SETTINGS.BUTTON_SETTINGS.addEventListener("click", function () {
-  closePopups(SETTINGS.SETTINGS_WRAPPER);
+    closePopups(SETTINGS.SETTINGS_WRAPPER);
 });
-
 SETTINGS.BUTTON_CLOSE_SETTINGS.addEventListener("click", function () {
-  closePopups(SETTINGS.SETTINGS_WRAPPER);
+    closePopups(SETTINGS.SETTINGS_WRAPPER);
 });
-
 ELEMENTS.BUTTON_EXIT.addEventListener("click", function () {
-  deleteAccountHistory();
-  closePopups(AUTHORIZATION.AUTHORIZATION_WRAPPER);
-});
-
-AUTHORIZATION.BUTTONS_GET_CODE.forEach((item) => {
-  item.addEventListener("click", function () {
+    deleteAccountHistory();
     closePopups(AUTHORIZATION.AUTHORIZATION_WRAPPER);
-    closePopups(CONFIRMATION.CONFIRMATION_WRAPPER);
-  });
 });
-
+AUTHORIZATION.BUTTONS_GET_CODE.forEach((item) => {
+    item.addEventListener("click", function () {
+        closePopups(AUTHORIZATION.AUTHORIZATION_WRAPPER);
+        closePopups(CONFIRMATION.CONFIRMATION_WRAPPER);
+    });
+});
 ELEMENTS.BUTTON_EXIT.addEventListener("click", function () {
-  AUTHORIZATION.AUTHORIZATION_WRAPPER.style.display = "flex";
+    AUTHORIZATION.AUTHORIZATION_WRAPPER.style.display = "flex";
 });
-
 export function closePopups(popup) {
-  const displayFlex = "display_flex"
-  if (!popup.classList.contains(displayFlex)) {
-    popup.classList.add(displayFlex);
-  } else {
-    popup.classList.remove(displayFlex);
-  }
+    const displayFlex = "display_flex";
+    if (!popup.classList.contains(displayFlex)) {
+        popup.classList.add(displayFlex);
+    }
+    else {
+        popup.classList.remove(displayFlex);
+    }
 }
-
 export function enterChat() {
-  closePopups(CONFIRMATION.CONFIRMATION_WRAPPER);
-  AUTHORIZATION.AUTHORIZATION_WRAPPER.style.display = "none";
+    closePopups(CONFIRMATION.CONFIRMATION_WRAPPER);
+    AUTHORIZATION.AUTHORIZATION_WRAPPER.style.display = "none";
 }
